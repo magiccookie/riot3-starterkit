8174a8d080e43d2ab036ce747b3f48e11b01593c
Magic Cookie <mgck@tuta.io>
2017-01-23 22:23:54 +0100

    Update to Riot 3.0

--- src/router.js
+++ src/router.js
@@ -1,7 +1,10 @@
 import './pages/home.tag'
 import './pages/projects.tag'
 
-// we need this to easily check the current route from every component 
+import route from 'riot-route';
+riot.route = route;
+
+// we need this to easily check the current route from every component
 riot.routeState = {
   view : ''
 };
@@ -12,19 +15,19 @@ class Router{
     this._currentView = null;
     this._views = ['home', 'projects'];
     this._defaultView = 'home';
-    
+
     riot.route(this._handleRoute.bind(this));
     riot.route.exec(this._handleRoute.bind(this));
   }
 
-  _handleRoute(view){
+  _handleRoute(view) {
 
     // load default view, if view is not in views list
     if(this._views.indexOf(view) === -1){
       return riot.route(this._defaultView);
     }
 
-    this._loadView(view); 
+    this._loadView(view);
   }
 
   _loadView(view){
